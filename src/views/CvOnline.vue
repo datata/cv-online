a948d1fafd2f4dc697e5b09abaf8a520216d2929
datata <dani@danitarazona.dev>
2021-07-29 16:56:17 +0200

    [IMP] Simple form added

--- src/views/CvOnline.vue
+++ src/views/CvOnline.vue
@@ -1,5 +1,42 @@
 <template>
   <div class="about">
     <h1>Start your cv online</h1>
+    <el-form ref="form" :model="form">
+      <el-form-item>
+        <el-input placeholder="Your name" v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input
+          placeholder="Your profession"
+          v-model="form.profession"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="Your email" v-model="form.email"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input placeholder="Your phone" v-model="form.phone"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        name: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log("submit!");
+    },
+  },
+};
+</script>
