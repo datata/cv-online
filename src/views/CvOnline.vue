4089a0f08db404bb4a5eceb7cefd53d74a997583
datata <dani@danitarazona.dev>
2021-08-02 16:34:26 +0200

    [IMP] Experiences added to cv form

--- src/views/CvOnline.vue
+++ src/views/CvOnline.vue
@@ -18,6 +18,38 @@
         <el-input placeholder="Your phone" v-model="form.phone"></el-input>
       </el-form-item>
       <el-form-item>
+        <el-input
+          placeholder="Your LinkedIn"
+          v-model="form.linkedin"
+        ></el-input>
+      </el-form-item>
+
+      <el-form-item
+        v-for="experience in form.experiences"
+        :label="'Experience'"
+        :key="experience.key"
+      >
+        <el-input :placeholder="'Dates'" v-model="experience.dates"></el-input>
+        <el-input :placeholder="'Job'" v-model="experience.job"></el-input>
+        <el-input
+          :placeholder="'Corporate'"
+          v-model="experience.corporate"
+        ></el-input>
+        <el-input
+          type="textarea"
+          autosize
+          placeholder="Description"
+          v-model="experience.description"
+        >
+        </el-input>
+        <el-button @click.prevent="removeExperience(experience)"
+          >Delete</el-button
+        >
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="addLaboralExperience">New experience</el-button>
+      </el-form-item>
+      <el-form-item>
         <el-button type="primary" @click="onSubmit">Create</el-button>
       </el-form-item>
     </el-form>
@@ -26,10 +58,10 @@
 </template>
 
 <script>
-import CvModelPaola from '@/components/CvModelPaola.vue';
+import CvModelPaola from "@/components/CvModelPaola.vue";
 
 export default {
-  components:{
+  components: {
     CvModelPaola,
   },
   data() {
@@ -40,6 +72,16 @@ export default {
         profession: "",
         email: "",
         phone: "",
+        linkedin: "",
+        experiences: [
+          {
+            key: 1,
+            corporate: "",
+            dates: "",
+            description: "",
+            job: "",
+          },
+        ],
       },
     };
   },
@@ -51,6 +93,18 @@ export default {
       setTimeout(() => {
         this.print = false;
       }, 100);
+    },
+    addLaboralExperience() {
+      this.form.experiences.push({
+        key: Date.now(),
+        value: "",
+      });
+    },
+    removeExperience(item) {
+      var index = this.form.experiences.indexOf(item);
+      if (index !== -1) {
+        this.form.experiences.splice(index, 1);
+      }
     },
   },
 };
